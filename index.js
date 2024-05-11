33b91e3d1b669c0a9b2f5aa95385bd7d2d224e29
Raphael Vitorio <raphaelvitorio01@gmail.com>
2024-05-11 13:01:25 -0300

    MERN Stack

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/soma', (req, res) => {
+    const soma = 100 + 1;
+    res.send({soma: soma})
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
